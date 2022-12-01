da2791cffa7ea7f51777369870d11daaab71e77c
Adrián Pavlik <pavlik.adrian1005@gmail.com>
2022-12-01 09:10:36 +0100

    ParentOptions API

---
+++ src/components/ParentOptionsApi.vue
@@ -0,0 +1,33 @@
+<!-- eslint-disable prettier/prettier -->
+<!-- SFC single file component -->
+<template>
+     <div>
+          <button @click="greet1()">Say hi!</button>
+          <button @click="greet2()">Say hi!</button>
+     </div>
+
+</template>
+
+<script>
+//options API
+export default {
+     name: "ParentOptionsApi",
+     data() {
+          return {
+               title: "Parent Options API",
+          };
+     },
+     methods: {
+          greet1() {
+               console.log(this.title);
+          },
+          greet2: () => {
+               console.log(this);
+          },
+     },
+};
+</script>
+
+<style scoped>
+
+</style>
